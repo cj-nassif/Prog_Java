23552c5f93ac7c4f80bec802c0c3d9978a6e418c
cj-nassif <nassif.cj@gmail.com>
2021-11-18 21:21:51 -0300

    Exercicio Java ProgI Aula2

--- src/massacorporalexemplo/MassaCorporalExemplo.java
+++ src/massacorporalexemplo/MassaCorporalExemplo.java
@@ -5,36 +5,89 @@ import java.util.Scanner;
 public class MassaCorporalExemplo {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double peso, alt, imc;
-        System.out.print("Peso...: ");
-        peso = Double.parseDouble(sc.nextLine());
-        System.out.print("Altura.: ");
-        alt = Double.parseDouble(sc.nextLine());
-        imc = peso / (alt * alt);
-        System.out.println("IMC..: " + imc);
         
-        if (imc < 18.5) {
-             System.out.println("Abaixo do peso");
+    //  Início Programa Calcula Média
 
-        }
-        else if (imc < 25) {
-             System.out.println("Peso Normal");
+        
+        Scanner sc = new Scanner(System.in);
 
-        } else if (imc < 30) {
-             System.out.println("Sobrepeso");
+double media, nota1, nota2;
 
-        } else if (imc < 35) {
-             System.out.println("Obesidade grau 1");
+System.out.println("Digite a nota 1:");
 
-        } else if (imc < 40) {
-             System.out.println("Obesidade Grau 2");
+nota1 = Double.parseDouble(sc.nextLine());
 
-        }
-        else {
-             System.out.println("Obesidade Grau 3");
+System.out.println("Digite a nota 2:");
 
-        }
+nota2 = Double.parseDouble(sc.nextLine());
+
+media = (nota1 + nota2) /2.0;
+
+System.out.println("A sua média é:" + media);
+
+sc.close();
+
+    //  Fim Programa Calcula Média
+
+        
+        //  Início Programa Verifica se numero entrado é primo
+//        Scanner sc = new Scanner(System.in);
+//        
+//        int nr, i, cont = 0;
+//        System.out.print("Número..: ");
+//        nr = Integer.parseInt(sc.nextLine());
+//        
+//        for (i = 1; i <= nr; i++) {
+//            if(nr % i == 0) {
+//                cont++;
+//            }
+//        }
+//        if (cont == 2) {
+//           System.out.print("--- O número é PRIMO ---");
+//
+//        }
+//        else {
+//           System.out.print("--- O número NÃO é primo ---");
+//
+//        }
+
+        //  Fim Programa Verifica se numero entrado é primo
+//        
+        
+
+        //  Início Programa Verifica IMC
+        
+//        double peso, alt, imc;
+//        System.out.print("Peso...: ");
+//        peso = Double.parseDouble(sc.nextLine());
+//        System.out.print("Altura.: ");
+//        alt = Double.parseDouble(sc.nextLine());
+//        imc = peso / (alt * alt);
+//        System.out.println("IMC..: " + imc);
+//        
+//        if (imc < 18.5) {
+//             System.out.println("Abaixo do peso");
+//
+//        }
+//        else if (imc < 25) {
+//             System.out.println("Peso Normal");
+//
+//        } else if (imc < 30) {
+//             System.out.println("Sobrepeso");
+//
+//        } else if (imc < 35) {
+//             System.out.println("Obesidade grau 1");
+//
+//        } else if (imc < 40) {
+//             System.out.println("Obesidade Grau 2");
+//
+//        }
+//        else {
+//             System.out.println("Obesidade Grau 3");
+//
+//        }
+        //  Fim Programa Verifica IMC
+
 
     }
 
